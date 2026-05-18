94b7d4009b6012c6075f4984237b240debe0f8de
Peter Muraya <sammypeter1944@gmail.com>
2026-05-18 22:38:09 +0300

    Serve Meta verification at exact HTML path

---
+++ functions/0nxwu7eh80lg2lb4s1p3h1k3pj6mnb.html.ts
@@ -0,0 +1,7 @@
+export const onRequest = () =>
+  new Response("0nxwu7eh80lg2lb4s1p3h1k3pj6mnb", {
+    headers: {
+      "Content-Type": "text/html; charset=utf-8",
+      "Cache-Control": "public, max-age=0, must-revalidate",
+    },
+  });
